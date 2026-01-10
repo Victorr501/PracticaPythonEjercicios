7c0f31d30404885940f9ce36dc3f15ae1d271481
Víctor Rubín Rubio <VrubinR501@outlook.com>
2026-01-10 16:56:20 +0100

    10/1/2026

--- Ejercicio033/ejercicio_33.py
+++ Ejercicio033/ejercicio_33.py
@@ -65,6 +65,13 @@ def crear_usuario_endpoint(service, data):
     except UsuarioDuplicadoError:
         return {"status_code": 409, "error": "Usuario duplicado"}
     
+def obtener_usuario_endpoint(service, nombre):
+    try:
+        usuario = service.obtener_usuario(nombre)
+        return {"status_code": 200, "data": usuario}
+    except UsuarioNoEncontradoError:
+        return {"status_code": 404, "error": "Usuario no encontrado"}
+    
     
 # Parte 2 - Repo: eliminar por nombre
 # 2 Implementa UsuarioRepository.eliminar_por_nombre(self, nombre):
@@ -74,3 +81,91 @@ def crear_usuario_endpoint(service, data):
 #       - hace return
 #   - Si no lo encuentra:
 #       -lanza UsuarioNoEncontradoError()
+
+class UsuarioRepository:
+    def __init__(self):
+        self._usuarios = []
+        
+    def agregar(self, usuario):
+        for usuario_buscar in self._usuarios:
+            if usuario.nombre == usuario_buscar.nombre:
+                raise UsuarioDuplicadoError()
+        self._usuarios.append(usuario)
+        
+    def buscar_por_nombre(self, nombre):
+        for usuario_buscar in self._usuarios:
+            if nombre == usuario_buscar.nombre:
+                return usuario_buscar
+        raise UsuarioNoEncontradoError()
+    
+    def eliminar_por_nombre(self, nombre):
+        index = -1
+        for usuario_buscar in self._usuarios:
+            index += 1
+            if nombre == usuario_buscar.nombre:
+                return self._usuarios.pop(index)
+        raise UsuarioNoEncontradoError()
+
+# Parte 3 - Service: eliminar usuario
+# 3 Implementa UsuarioService.eliminar_usuario(self, nombre):
+# - Llama a repo.eliminar_por_nombre(nombre)
+# - No devuelve nada(None)
+
+class UsuarioService:
+    def __init__(self, repo):
+        self.repo = repo
+    
+    def crear_usuario(self, data):
+        usuario = usuario_desde_dict(data)
+        self.repo.agregar(usuario)
+        return usuario.to_dict()
+    
+    def obtener_usuario(self, nombre):
+        usuario = self.repo.buscar_por_nombre(nombre)
+        return usuario.to_dict()
+    
+    def eliminar_usuario(self, nombre):
+        usuario = self.repo.eliminar_por_nombre( nombre)
+        return None
+    
+# Parte 4 - Endpoint DELETE simulado
+# 4. Crear una función eliminar_usuario_endpoint(service, nombre):
+# Si se elimina correctamente:
+# {"status_code": 204}
+# Si no existe(UsuarioNoEncontradoError):
+# {"status_code": 404, "error": "Usuario no encontrado"}
+
+def eliminar_usuario_endpoint(service, nombre):
+    try:
+        service.eliminar_usuario(nombre)
+        return {"status_code": 204}
+    except UsuarioNoEncontradoError:
+        return {"status_code": 404, "error": "Usuario no encontrado"}
+    
+# Parte 5 - Pruebas
+# 5 Inicializa repo + service
+# 6 Crea 2 usuarios con crear_usuario_endpoint
+# 7 Prueba:
+#   - Eliminar un usuario existen -> imprimir respuesta
+#   - Intenar eliminar el mismo usuario otra vez -> imprimir repuesta (debe ser 404)
+#   - confirmar que el otro usuario sigue existiendo usando obtener_usuario_endpoint
+
+usuarioRepository = UsuarioRepository()
+usuarioService = UsuarioService(usuarioRepository)
+
+data1 = {"nombre": "Victor", "edad": 20, "activo": True}
+data2 = {"nombre": "Lorena", "edad": 20, "activo": True}
+
+crear_usuario_endpoint(usuarioService, data1)
+crear_usuario_endpoint(usuarioService, data2)
+
+respuesta1 = eliminar_usuario_endpoint(usuarioService, "Victor")
+respuesta2 = eliminar_usuario_endpoint(usuarioService, "Victor")
+
+print(respuesta1)
+print(respuesta2)
+
+respuesta3 = obtener_usuario_endpoint(usuarioService, "Lorena")
+print(respuesta3)
+                
+            
